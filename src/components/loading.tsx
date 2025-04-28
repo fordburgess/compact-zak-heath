d033d756a4710691ec74b419b9b481a92b6c71eb
ford burgess <fburg6824@gmail.com>
2025-04-28 20:40:47 +0200

    added loader - dont know if ill keep it

---
+++ src/components/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/loading.css'
+
+const Loading = () => {
+  return (
+    <div className='loading-container'>
+      <span className='loader'></span>
+      <p>Loading</p>
+    </div>
+  )
+}
+
+export default Loading
