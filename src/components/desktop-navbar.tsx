ef4ac6813fb38b9d1d4a764fff83861fe76d6cdb
ford burgess <fburg6824@gmail.com>
2025-04-29 15:18:44 +0200

    added navbar for desktop

--- src/components/desktop-navbar.tsx
+++ src/components/desktop-navbar.tsx
@@ -1,40 +1,22 @@
-import React, { useEffect } from 'react';
+"use client"
+import React from 'react';
 import Link from 'next/link';
 import './styles/desktop-navbar.css';
 import gsap from 'gsap';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 
 const DesktopNavbar = () => {
-  const router = useRouter();
-  const { pathname } = router;
-
-  const handleHover = () => {
-    gsap.to('.nav-link-container', {
-      y: 0,
-      ease: "power1.in",
-      duration: 0.2
-    })
-  }
-
-  const handleExit = () => {
-    gsap.to('.nav-link-container', {
-      y: 100,
-      ease: "power1.in",
-      duration: 0.2
-    })
-  }
-
-  // need to fix link styles
+  const pathname = usePathname();
 
   return (
-    <div className='nav-hover-container' onMouseEnter={() => handleHover()} onMouseLeave={() => handleExit()}>
+    <div className='nav-hover-container'>
       <div className='nav-link-container'>
-        <Link className={pathname.includes('/services') ? 'active' : ''} href='/services'>Services</Link>
-        <Link className={pathname.includes('/audio') ? 'active' : ''} href='/audio'>Audio</Link>
-        <Link className={pathname.includes('/dream-room') ? 'active' : ''} href='/dream-room'>Dream Room</Link>
-        <Link className={pathname == '/' ? 'active' : ''} href='/'>Home</Link>
-        <Link className={pathname.includes('/featured') ? 'active' : ''} href='/featured'>Featured</Link>
-        <Link className={pathname.includes('/beauty-icons') ? '' : ''} href='/beauty-icons'>Beauty Icons</Link>
+        <Link className={pathname == '/' ? 'active nav-link' : 'nav-link'} href='/'>Home</Link>
+        <Link className={pathname.includes('/featured') ? 'active nav-link' : 'nav-link'} href='/featured'>Featured</Link>
+        <Link className={pathname.includes('/audio') ? 'active nav-link' : 'nav-link'} href='/audio'>Podcasts</Link>
+        <Link className={pathname.includes('/beauty-icons') ? '' : 'nav-link'} href='/beauty-icons'>Beauty Icons</Link>
+        <Link className={pathname.includes('/dream-room') ? 'active nav-link' : 'nav-link'} href='/dream-room'>Dream Room</Link>
+        <Link className={pathname.includes('/services') ? 'active nav-link' : 'nav-link'} href='/services'>Services</Link>
       </div>
     </div>
   )
