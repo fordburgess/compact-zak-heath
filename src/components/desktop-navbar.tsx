75b0d0699b83ab5f506e6eeafec8ee9890da32ad
ford burgess <fburg6824@gmail.com>
2025-04-29 17:12:23 +0200

    testing prefetch

--- src/components/desktop-navbar.tsx
+++ src/components/desktop-navbar.tsx
@@ -11,12 +11,12 @@ const DesktopNavbar = () => {
   return (
     <div className='nav-hover-container'>
       <div className='nav-link-container'>
-        <Link className={pathname == '/' ? 'active nav-link' : 'nav-link'} href='/'>Home</Link>
-        <Link className={pathname.includes('/featured') ? 'active nav-link' : 'nav-link'} href='/featured'>Featured</Link>
-        <Link className={pathname.includes('/audio') ? 'active nav-link' : 'nav-link'} href='/audio'>Podcasts</Link>
-        <Link className={pathname.includes('/beauty-icons') ? 'active nav-link' : 'nav-link'} href='/beauty-icons'>Beauty Icons</Link>
-        <Link className={pathname.includes('/dream-room') ? 'active nav-link' : 'nav-link'} href='/dream-room'>Dream Room</Link>
-        <Link className={pathname.includes('/services') ? 'active nav-link' : 'nav-link'} href='/services'>Services</Link>
+        <Link prefetch={true} className={pathname == '/' ? 'active nav-link' : 'nav-link'} href='/'>Home</Link>
+        <Link prefetch={true} className={pathname.includes('/featured') ? 'active nav-link' : 'nav-link'} href='/featured'>Featured</Link>
+        <Link prefetch={true} className={pathname.includes('/audio') ? 'active nav-link' : 'nav-link'} href='/audio'>Podcasts</Link>
+        <Link prefetch={true} className={pathname.includes('/beauty-icons') ? 'active nav-link' : 'nav-link'} href='/beauty-icons'>Beauty Icons</Link>
+        <Link prefetch={true} className={pathname.includes('/dream-room') ? 'active nav-link' : 'nav-link'} href='/dream-room'>Dream Room</Link>
+        <Link prefetch={true} className={pathname.includes('/services') ? 'active nav-link' : 'nav-link'} href='/services'>Services</Link>
       </div>
     </div>
   )
