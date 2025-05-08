85ff248dcd6a8525e8672466c51c243f1bdbdf6e
ford burgess <fburg6824@gmail.com>
2025-05-08 22:23:07 +0200

    updated href to lead to articles instead of featured

--- src/components/desktop-navbar.tsx
+++ src/components/desktop-navbar.tsx
@@ -12,7 +12,7 @@ const DesktopNavbar = () => {
     <div className='nav-hover-container'>
       <div className='nav-link-container'>
         <Link prefetch={true} className={pathname == '/' ? 'active nav-link' : 'nav-link'} href='/'>Home</Link>
-        <Link prefetch={true} className={pathname.includes('/featured') ? 'active nav-link' : 'nav-link'} href='/featured'>Featured</Link>
+        <Link prefetch={true} className={pathname.includes('/articles') ? 'active nav-link' : 'nav-link'} href='/articles'>Featured</Link>
         <Link prefetch={true} className={pathname.includes('/audio') ? 'active nav-link' : 'nav-link'} href='/audio'>Podcasts</Link>
         <Link prefetch={true} className={pathname.includes('/beauty-icons') ? 'active nav-link' : 'nav-link'} href='/beauty-icons'>Beauty Icons</Link>
         <Link prefetch={true} className={pathname.includes('/dream-room') ? 'active nav-link' : 'nav-link'} href='/dream-room'>Dream Room</Link>
